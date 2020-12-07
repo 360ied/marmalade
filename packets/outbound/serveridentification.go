f3a7c728c6e1287cb8fbff47d606a1b25cf58ac5
rian <19516527+360ied@users.noreply.github.com>
2020-12-06 22:50:22 -0500

    send protocol version

--- packets/outbound/serveridentification.go
+++ packets/outbound/serveridentification.go
@@ -2,6 +2,7 @@ package outbound
 
 func (w *AFCBW) SendServerIdentification(serverName, serverMOTD string, isOP bool) error {
 	return w.do(writeByte(0x00), // packet id
+		writeByte(0x07), // protocol version
 		writeString(serverName),
 		writeString(serverMOTD),
 		writeByte(opByte(isOP)))
